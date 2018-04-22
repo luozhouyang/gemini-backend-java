6859c4d41aca801f8d3221ec4684320fde5ddc63
luozhouyang <stupidme.me.lzy@gmail.com>
2018-04-22 21:27:40 +0800

    Create user dao

--- src/main/java/me/stupidme/gemini/dao/UserDao.java
+++ src/main/java/me/stupidme/gemini/dao/UserDao.java
@@ -1,4 +1,48 @@
 package me.stupidme.gemini.dao;
 
+import me.stupidme.gemini.domain.User;
+
+import java.sql.*;
+import java.util.Calendar;
+
 public class UserDao {
+
+    private static final String SQL_INSERT = "INSERT INTO users (uuid, name, email, password, created_at) " +
+            "VALUES (?, ?, ?, ?, ?) RETURNING id, created_at";
+
+    public UserDao() {
+
+    }
+
+    public boolean insert(User user) {
+        Connection connection = ConnectionPool.getInstance().getConnection();
+        if (connection == null) {
+            return false;
+        }
+        try {
+            if (user.getCreatedAt() == null) {
+                Calendar calendar = Calendar.getInstance();
+                long time = calendar.getTimeInMillis();
+                user.setCreatedAt(new Timestamp(time));
+            }
+            PreparedStatement statement = connection.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, createUuid(user));
+            statement.setString(2, user.getUserName());
+            statement.setString(3, user.getEmail());
+            statement.setString(4, user.getPassword());
+            statement.setTimestamp(5, user.getCreatedAt());
+            ResultSet set = statement.getGeneratedKeys();
+            user.setId(set.getLong(1));
+            user.setCreatedAt(set.getTimestamp(2));
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private String createUuid(User user) {
+        return "";
+    }
+
 }
--- src/main/java/me/stupidme/gemini/domain/User.java
+++ src/main/java/me/stupidme/gemini/domain/User.java
@@ -1,4 +1,61 @@
 package me.stupidme.gemini.domain;
 
+import java.sql.Timestamp;
+
 public class User {
+    private long id;
+    private String userName;
+    private String uuid;
+    private String email;
+    private String password;
+    private Timestamp createdAt;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
