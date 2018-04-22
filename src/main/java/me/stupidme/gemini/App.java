b9030c75d23ab48082f9b1ef21d0e09fe89881af
luozhouyang <stupidme.me.lzy@gmail.com>
2018-04-22 18:30:42 +0800

    Create servlets

---
+++ src/main/java/me/stupidme/gemini/App.java
@@ -0,0 +1,4 @@
+package me.stupidme.gemini;
+
+public class App {
+}
---
+++ src/main/java/me/stupidme/gemini/servlets/LoginServlet.java
@@ -0,0 +1,28 @@
+package me.stupidme.gemini.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+        PrintWriter writer = response.getWriter();
+        writer.write("Method not allowed.");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter writer = response.getWriter();
+        writer.write("Server have not ready for your post request.\n");
+    }
+}
