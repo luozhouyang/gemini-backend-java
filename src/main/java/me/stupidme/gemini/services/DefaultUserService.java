4a83561479c15e86bbe8b30abb568b42aeb98c98
luozhouyang <stupidme.me.lzy@gmail.com>
2018-04-22 21:27:50 +0800

    Create user service

--- src/main/java/me/stupidme/gemini/services/DefaultUserService.java
+++ src/main/java/me/stupidme/gemini/services/DefaultUserService.java
@@ -1,4 +1,51 @@
 package me.stupidme.gemini.services;
 
-public class DefaultUserService {
+import me.stupidme.gemini.domain.User;
+
+import java.util.List;
+
+/**
+ * A default implementation of UserService.
+ */
+public class DefaultUserService implements UserService {
+
+    @Override
+    public boolean insertUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean updateUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean updateOrInsert(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteUser(User user) {
+        return false;
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User findUserById(long id) {
+        return null;
+    }
+
+    @Override
+    public User findUserByUuid(String uuid) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return null;
+    }
 }
--- src/main/java/me/stupidme/gemini/services/UserService.java
+++ src/main/java/me/stupidme/gemini/services/UserService.java
@@ -1,4 +1,74 @@
 package me.stupidme.gemini.services;
 
-public class UserService {
+import me.stupidme.gemini.domain.User;
+
+import java.util.List;
+
+/**
+ * Define all operations to User.
+ */
+public interface UserService {
+
+    /**
+     * Insert a User.
+     *
+     * @param user 　the user
+     * @return true if insertion is success, or false
+     */
+    boolean insertUser(User user);
+
+    /**
+     * Update a user.
+     *
+     * @param user user
+     * @return true if update is success, or false
+     */
+    boolean updateUser(User user);
+
+    /**
+     * Update a user, if user does not exist then insert one.
+     *
+     * @param user user
+     * @return true if update or insertion is success, or false.
+     */
+    boolean updateOrInsert(User user);
+
+    /**
+     * Delete a user.
+     *
+     * @param user user
+     * @return true if deletion is success, or false.
+     */
+    boolean deleteUser(User user);
+
+    /**
+     * Find user by email.
+     *
+     * @param email email
+     * @return user if found, or null
+     */
+    User findUserByEmail(String email);
+
+    /**
+     * Find user by user's id.
+     *
+     * @param id id
+     * @return user if found, or null
+     */
+    User findUserById(long id);
+
+    /**
+     * Find user by uuid.
+     *
+     * @param uuid uuid
+     * @return user if found, or null
+     */
+    User findUserByUuid(String uuid);
+
+    /**
+     * Find all users.
+     *
+     * @return users list if have any, or empty list
+     */
+    List<User> findAllUsers();
 }
